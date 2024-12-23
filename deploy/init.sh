ecac3ee18ea2f3f4b77b587642c4ac606f4f7071
Tahir Ali <tahir.ali@coeus-solutions.de>
2024-12-23 19:58:43 +0500

    update gh action

--- deploy/init.sh
+++ deploy/init.sh
@@ -5,12 +5,6 @@ set -e
 
 echo "Running database migrations..."
 
-# Activate virtual environment if it exists
-if [ -d "venv" ]; then
-    source venv/bin/activate
-fi
-
-# Run migrations using Alembic
 alembic upgrade head
 
 echo "Migrations completed successfully!" 
